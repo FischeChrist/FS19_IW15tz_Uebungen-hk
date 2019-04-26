0fb9b6375b9ddc4724dcd5df9b0994e20fe1386c
FischeChrist <christina.fischer@iw.htwchur.ch>
2019-04-26 10:26:09 +0200

    Various tests with object methods

--- 06_objectorientedJavascript/06_Starterpackage/script.js
+++ 06_objectorientedJavascript/06_Starterpackage/script.js
@@ -1,11 +1,22 @@
 var person = {
-	firstName : "Christina";
-	lastName : "Fischer";
-	age : 33;
-	interests : ["reading", "cooking"];
-	message : function(
-		alert("Hi, my name is " + firstName + " " + lastName);
-		)
+	firstName : "Christina",
+	lastName : "Fischer",
+	age : function() {
+		//year = new Date();
+		//year = 2019;
+		//myYear = 1986;
+		//myAge = ((year.getFullYear()) - this.myYear);
+		//myAge = (this.year - this.myYear);
+		myAge = 2019-1986;
+		alert("My age is " + this.myAge);
+	},
+	interests : ["reading", "cooking"],
+	greeting : function() {
+		alert("Hi, my name is " + this.firstName + " " + this.lastName);
+		}
 }
 
-alert((new Date()).getFullYear());
+
+console.log("Hab's geladen.");
+
+console.log(2019-1986);
