391a669ffb940829b7ed2ec8bc82fff3508a5243
FischeChrist <christina.fischer@iw.htwchur.ch>
2019-04-26 09:24:39 +0200

    Added person as object

---
+++ 06_objectorientedJavascript/06_Starterpackage/script.js
@@ -0,0 +1,9 @@
+var person = {
+	firstName : "Christina";
+	lastName : "Fischer";
+	age : 33;
+	interests : ["reading", "cooking"];
+	message : function(
+		alert("Hi, my name is " + firstName + " " + lastName);
+		)
+}
